32389c53ae58f6371e285e841e7e8749bb0a5410
macbook <macbook@macbooks-MacBook-Pro.local>
2025-06-04 11:54:25 +0700

    update5

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
